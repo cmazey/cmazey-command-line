1f3cbfee4fd4036cd80a89e0817e210176387643
cmazey <cmazey10@outlook.com>
2023-10-28 02:29:07 -0400

    updated startup.py (math)

--- Cmd-REPL/re/math/startup.py
+++ Cmd-REPL/re/math/startup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 
 
@@ -15,10 +16,19 @@ time.sleep(0.1)
 print("LOADING COMPLETED")
 time.sleep(1)
 print("Loading Repl...")
-time.sleep(2)
-print("Loading Repl completed")
+animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+
+for i in range(len(animation)):
+    time.sleep(1.1)
+    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.flush()
+
+print("\n")
+print("REPL LOADED! Please hold...")
 time.sleep(3)
 print("ENJOY MATH Y'ALL NERDS!")
+
+
 
 time.sleep(0.5)
 
