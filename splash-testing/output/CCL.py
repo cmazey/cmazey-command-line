a053c59298c901152f98a565d4c774e3f70dac31
cmazey <Cmazey10@outlook.com>
2024-02-29 13:34:25 -0500

    updated CCL.py (splash testing)

--- splash-testing/output/CCL.py
+++ splash-testing/output/CCL.py
@@ -6,6 +6,12 @@ import inspect
 current_script_path = inspect.getfile(inspect.currentframe())
 current_directory = os.path.dirname(current_script_path)
 
-os.system('python .\\Cmd-REPL\\ccl.py')
+os.chdir('Cmd-REPL')
+
+f = open("CCLSTARTUP.txt", "w")
+f.write("This is used to access Cmazey Command Line, it will be deleted if it has successfully opened the file...")
+f.close()
+
+os.system('python ccl.py')
 
 
